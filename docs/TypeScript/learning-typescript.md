a6384e4de9381c439a0719c8944bf50198460ca7
Piscoo <piscosmith@foxmail.com>
2022-03-15 23:49:38 +0800

    目录搭建

---
+++ docs/TypeScript/learning-typescript.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+---
+# TypeScript 入门
+<!-- # Learning TypeScript -->
+# Notes while reading TypeScript Documents
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,105 +6,96 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Pisco's Blog",
-	tagline: 'Welcome to my website!',
-	url: 'https://your-docusaurus-test-site.com',
-	baseUrl: '/',
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/favicon.ico',
-	organizationName: 'Pisco', // Usually your GitHub org/user name.
-	projectName: "Pisco's Blog", // Usually your repo name.
+    title: "Pisco's Blog",
+    tagline: 'Welcome to my website!',
+    url: 'https://github.com/Piscoo',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'Pisco', // Usually your GitHub org/user name.
+    projectName: "Pisco's Blog", // Usually your repo name.
 
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			}),
-		],
-	],
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: 'Pisco',
-				logo: {
-					alt: 'My Site Logo',
-					src: 'img/gd.jpeg',
-				},
-				items: [
-					{
-						type: 'doc',
-						docId: 'intro',
-						position: 'right',
-						label: 'Tutorial',
-					},
-					{
-						label: 'Blog',
-						to: '/blog',
-						position: 'right'
-					},
-					{
-						label: 'GitHub',
-						href: 'https://github.com/Piscoo',
-						position: 'right',
-					},
-				],
-			},
-			footer: {
-				style: 'dark',
-				links: [
-					{
-						title: 'Docs',
-						items: [
-							{
-								label: 'Tutorial',
-								to: '/docs/intro',
-							},
-						],
-					},
-					{
-						title: 'Blog',
-						items: [
-							{
-								label: 'Blog',
-								to: '/blog',
-							},
-						],
-					},
-					{
-						title: 'More',
-						items: [
-							{
-								label: 'GitHub',
-								href: 'https://github.com/Piscoo',
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-		}),
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+        navbar: {
+            title: 'Pisco',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/gd.jpeg',
+            },
+            items: [{
+                    type: 'doc',
+                    docId: 'intro',
+                    position: 'right',
+                    label: 'Tutorial',
+                },
+                {
+                    label: 'Blog',
+                    to: '/blog',
+                    position: 'right'
+                },
+                {
+                    label: 'GitHub',
+                    href: 'https://github.com/Piscoo',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [{
+                    title: 'Docs',
+                    items: [{
+                        label: 'Tutorial',
+                        to: '/docs/intro',
+                    }, ],
+                },
+                {
+                    title: 'Blog',
+                    items: [{
+                        label: 'Blog',
+                        to: '/blog',
+                    }, ],
+                },
+                {
+                    title: 'More',
+                    items: [{
+                        label: 'GitHub',
+                        href: 'https://github.com/Piscoo',
+                    }, ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+    }),
 };
 
 module.exports = config;
